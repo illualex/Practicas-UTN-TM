42ba1a58d64b1ca253d77d56186a5af04e833efc
Alejo <alejooodaniel45@gmail.com>
2024-07-02 05:22:06 -0300

    Subida de archivos de la clase 22 (Practicas)

---
+++ clase-22/colors-cards/src/App.jsx
@@ -0,0 +1,49 @@
+// App.jsx
+import React from 'react';
+import './App.css';
+import Card from './components/Card/Card';
+
+function App() {
+  const tarjetas = [
+    {
+      colors: ["#000000", "#FF4191", "#E90074", "#FFF078"],
+      likes: 311,
+      update: "5 días",
+      id: 0,
+    },
+    {
+      colors: ["#EEEDEB", "#E6B9A6", "#939185", "#2F3645"],
+      likes: 567,
+      update: "6 días",
+      id: 1,
+    },
+    {
+      colors: ["#36BA98", "#E9C46A", "#F4A261", "#E76F51"],
+      likes: 504,
+      update: "1 semana",
+      id: 2,
+    },
+    {
+      colors: ["#667BC6", "#FDFFD2", "#FFB4C2", "#DA7297"],
+      likes: 682,
+      update: "1 semana",
+      id: 3,
+    },
+  ];
+
+  return (
+    <div className="app">
+      {tarjetas.map((tarjeta) => (
+        <Card
+          key={tarjeta.id}
+          colors={tarjeta.colors}
+          likes={tarjeta.likes}
+          update={tarjeta.update}
+          id={tarjeta.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ clase-22/colors-cards/src/components/Card/Card.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import LikeButton from '../Button/Button';
+import './Card.css';
+
+const Card = ({ colors, likes, update, id }) => {
+    /* Estado para almacenar el color sobre donde se coloque el mouse*/
+    const [hoveredColor, setHoveredColor] = useState(null);
+
+    /* Hace la acción de copiar color y muestra mensaje de alerta de copiado*/
+    const handleCopyColor = (color) => {
+        navigator.clipboard.writeText(color);
+        window.alert('¡Color copiado!');
+    };
+
+    return (
+        <div className="card">
+            <div className="color-container">
+                {colors.map((color, index) => (
+                    <div
+                        key={index}
+                        className="color"
+                        /* Muestra el color del App.jsx */
+                        style={{ backgroundColor: color }}
+                        /* Muestra el color a colocar el mouse */
+                        onMouseEnter={() => setHoveredColor(color)}
+                        /* Quita el mensaje del color */
+                        onMouseLeave={() => setHoveredColor(null)}
+                        /* Acción para copiar el código del color seleccionado */
+                        onClick={() => handleCopyColor(color)}
+                    >
+                        {/* Muestra el color del estado hover*/}
+                        {hoveredColor === color && (
+                            <span className="color-name">{color}</span>
+                        )}
+                    </div>
+                ))}
+            </div>
+            <div className="info">
+                {/* Importa componente btn */}
+                <LikeButton likes={likes} />
+                <p>{update}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
